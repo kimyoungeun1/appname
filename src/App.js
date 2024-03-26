3ef88693ab5f236b423d8d9f850dacdb7fc2b7ca
김영은 <kimyongen1607@gmail.com>
2024-03-26 17:12:24 +0900

    변수 변화 프로그램

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
+
 function App() {
+  const [snumber, setsnumber] = useState(0);
+  const rnumber = useRef(0);
+
+  const snumberup = () =>{
+    setsnumber(snumber+1);
+  }
+  
+  const rnumberup = () =>{
+    rnumber.current = rnumber.current + 1;
+    console.log('rnumber', rnumber.current);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick = {snumberup}>useState로 변수를 +1</button>
+      <h1>useState로 관리되는 변수는 {snumber}입니다.</h1>
+      <p>상태 증가와 함게 렌더링도 된다.</p>
+      <hr></hr>
+
+      <button onClick = {rnumberup}>useRef로 변수를 + 1</button>
+      <h1>useRef로 관리되는 변수는 {rnumber.current}입니다.</h1>
+      <p>변수 변화는 되지만, 렌더링은 안됩니다.</p>
+      <p>즉, 계산은 하지만, 레더링 시키고 싶지 않을 경우에 사용</p>
     </div>
   );
 }
